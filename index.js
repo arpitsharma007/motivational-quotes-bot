a3022379c26cdbcbc398511966cb7eafe2647b53
arpit sharma <hi@arpitsharma.in>
2021-09-24 04:31:43 +0530

    First-commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv').config()
+const Discord = require("discord.js")
+const fetch = require("node-fetch");
+
+const client = new Discord.Client({
+    partials: ["CHANNEL", "MESSAGE"],
+    restTimeOffset: 0,
+    intents: 513,
+})
+
+const sadWords = ["sad", "depressed", "unhappy", "angry"];
+
+const encouragements = ["Cheer up!", "Hang in there!", "You are a great person", "Death is ultimate truth my friend :p"]
+
+const getQuote = () => {
+    return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+        return res.json()
+    })
+    .then(data=> {
+        return data[0]["q"] + " " + "-" + " " + data[0]["a"]
+    })
+}
+
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("messageCreate", (msg) => {
+    if (msg.author.bot) return
+    if(msg.content.startsWith("$inspire")) {
+        getQuote().then(quote => msg.channel.send({content: quote,}))
+    }
+
+    if(sadWords.some(word => msg.content.includes(word))) {
+        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
+        msg.channel.send({
+            content: encouragement,
+        })
+    }
+
+    if(msg.content.startsWith("ping")) {
+        msg.channel.send({
+            content: 'pong',
+        })
+    }
+})
+
+console.log(process.env.TOKEN)
+
+client.login(process.env.TOKEN)
